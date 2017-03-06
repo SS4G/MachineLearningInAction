63f27b4a02c37a96c9ff57f89d77223957e21220
SS4G <ziheng_song@126.com>
2017-03-06 22:18:33 +0800

    ploter unsloved

--- plotertool/TreePloter.py
+++ plotertool/TreePloter.py
@@ -7,16 +7,22 @@ class TreePloter:
     """
     def __init__(self,level_height=10):
         self.figure_name="default tree"
-
+        self.level_depth=10  # 每个层级在图上的深度
+        self.figure_width=0  # 画布横向范围
+        self.figure_depth=0  # 画布纵向范围
+        self.h_scale=10      # 画布横向分度值
+        self.v_scale=10      # 画布纵向分度值
+        self.level_width=[]  # 每一level对应多少的node
+        self.level_used=[]   # 每一个level已经标记过的node数目 供层次遍历做记录使用
     def get_tree_depth(self,json_obj):
         """
         tree is in json format object
-        :param json_tree:
+        :param json_obj:
         :return: depth of the tree
         """
         for key in json_obj:  # only one key in each json obj
             next_objs=json_obj[key]  # get list of next level json objs
-            if next_objs==None:
+            if next_objs is None:
                 return 0
             else :
                 next_level_depth=[]
@@ -27,12 +33,12 @@ class TreePloter:
     def get_tree_width(self,json_obj):
         """
         只是一个大概的宽度 以一颗树的所有叶子节点之和作为一颗树的宽度
-        :param json_tree:
+        :param json_obj:
         :return:amount of leaf node num
         """
         for key in json_obj:  # only one key in each json obj
             next_objs = json_obj[key]  # get list of next level json objs
-            if next_objs==None:
+            if next_objs is None:
                 return 1  #if the json obj is leaf
             else:
                 sums=0
@@ -49,24 +55,48 @@ class TreePloter:
         json_obj_keys = json_obj.keys()
         for key in json_obj_keys:  # only one key in each json obj
             next_objs = json_obj[key]
-            if next_objs==None:
+            if next_objs is None:
                 return 0
-            if level==0:
-                return 1#only for root to get here
-            if level==1:#只询问下一级
+            if level == 0:
+                return 1  # only for root to get here
+            if level == 1:  # 只询问下一级
                 return len(next_objs)
-            else :
+            else:
                 sums=0
                 for next_obj in next_objs:
                     sums+=self.get_this_level_width(next_obj,level-1)
                 return sums
 
+    def mark_point(self,json_obj,this_level,level_width,level_used):
+        for key in json_obj:  # only one key
+            node_name=key
+            position=level_used[this_level]+1 #在该层第几个位置
+            level_used[this_level]+=1 # 将层级使用计数器加一
+            x=position*self.h_scale
+            y=this_level*self.v_scale
+            son_list=json_obj[key]
+
+            for son in son_list:
+                pass
     def plot_main(self,json_obj):
+        # 获取树的一些信息
         depth=self.get_tree_depth(json_obj)
         width=self.get_tree_width(json_obj)
+        self.level_width=[] # 每一level对应多少的node
+        self.level_used=[0,]*depth #每一个level使用掉的位置
+        for each_level in range(depth + 1):
+            self.level_width.append(tp.get_this_level_width(tree_root, each_level))
+
+        self.figure_width=(width-1)*self.h_scale  # 计算出整个画图范围的横向范围
+        self.figure_depth=depth*self.v_scale      # 计算出整个画图范围的纵向范围
+
+
+        self.mark_point(json_obj, 0) # 从根节点 层次0 开始进行层次化遍历
+
         plt.figure(self.figure_name,figsize=(10,10))
         plt.subplot(111)
         plt.axis([0,depth,0,width])
+
 if __name__=="__main__":
     tree_json=a="""
                 {"n00": [
@@ -99,6 +129,6 @@ if __name__=="__main__":
     each_level_width=[]
     for level in range(tree_depth+1):
         each_level_width.append(tp.get_this_level_width(tree_root,level))
-    print("tree_depth      =",tree_depth        )
-    print("tree_width      =",tree_width        )
+    print("tree_depth      =",tree_depth)
+    print("tree_width      =",tree_width)
     print("each_level_width=",each_level_width)
